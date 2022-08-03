917cf2b127357d84b7cc66f5198c7dd10fcd750e
Ahmad Bilal Ameen <iambilalameen@gmail.com>
2022-08-04 02:02:49 +0500

    Vue App File Created

---
+++ resources/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="text-center" style="margin: 20px 0px 20px 0px;">
+            <span class="text-secondary">Laravel Vue Crud</span>
+        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse">
+                <div class="navbar-nav">
+                    <route-link to="/" class="nav-item nav-link">Home</route-link>
+                    <route-link to="/add" class="nav-item nav-link">Add Post</route-link>
+                </div>
+            </div>
+        </nav>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+    export default {}
+</script>
